725d6dbaf3345a8978f73d78b6775e870fa45034
HuPengsheet <1554414201@qq.com>
2026-03-15 01:32:40 +0800

    add code

--- main.py
+++ main.py
@@ -19,8 +19,10 @@ class MyPlugin(Star):
         message_str = event.message_str # 用户发的纯文本消息字符串
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
-        create_record("LlTMbSY1xaAnIGs2oJ0ccdtenNf","tblcPLKRgKtQrBJW",{"名字":message_str})
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        url = message_str.split(" ")
+        if(len(url)<1):
+            return 
+        yield event.plain_result(f"Hello, {user_name}, 你发了 {url[1]}!") # 发送一条纯文本消息
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
